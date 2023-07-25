005aa88e160a128f8677d31feb3d4dc1cb7fce28
dsttl3 <admin@dsttl3.cn>
2023-07-25 15:27:16 +0800

    -

--- src/main/java/cn/dsttl3/ddns/util/ip/GetIPv4.java
+++ src/main/java/cn/dsttl3/ddns/util/ip/GetIPv4.java
@@ -1,12 +1,10 @@
 package cn.dsttl3.ddns.util.ip;
-
-
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
 
-public class GetIPv6 {
+public class GetIPv4 {
     public static void main(String[] args) {
         try {
             Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
@@ -15,10 +13,8 @@ public class GetIPv6 {
                 Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
                 while (inetAddresses.hasMoreElements()) {
                     InetAddress inetAddress = inetAddresses.nextElement();
-                    if (inetAddress instanceof java.net.Inet6Address) {
-                        if (inetAddress.getHostAddress().startsWith("240e") && inetAddress.getHostAddress().endsWith("%en0")) {
-                            System.out.println("IPv6 address: " + inetAddress.getHostAddress());
-                        }
+                    if (inetAddress instanceof java.net.Inet4Address) {
+                        System.out.println("IPv4 address: " + inetAddress.getHostAddress());
                     }
                 }
             }
