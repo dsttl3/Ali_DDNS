fb1ef7052899220a42f9d082504c1604987d8dd5
dsttl3 <admin@dsttl3.cn>
2023-07-31 15:49:07 +0800

    -

--- src/main/java/cn/dsttl3/ddns/util/Config.java
+++ src/main/java/cn/dsttl3/ddns/util/Config.java
@@ -10,9 +10,9 @@ public class Config {
     // 地域ID
     public static String  Region_ID = "cn-qingdao";
     // 您的 阿里云 AccessKey ID
-    public static String  AccessKey_ID = "";
+    public static String  AccessKey_ID = "******";
     // 您的 阿里云 AccessKey Secret
-    public static String  AccessKey_Secret = "";
+    public static String  AccessKey_Secret = "*****";
 
     public static String prefix = "240e";
 
